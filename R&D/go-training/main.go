48f1a44b9fd94e4dd65985d36b071a4a99ba3b84
Ken Wong <kenono2000@gmail.com>
2026-09-20 08:57:25 -0500

    Update

--- R&D/go-training/main.go
+++ R&D/go-training/main.go
@@ -2,6 +2,28 @@ package main
 
 import "fmt"
 
+func average(numbers []float64) (float64, error) {
+	if len(numbers) == 0 {
+		return 0, fmt.Errorf("cannot calculate average of empty slice")
+	}
+
+	var total float64
+
+	for _, number := range numbers {
+		total += number
+	}
+
+	return total / float64(len(numbers)), nil
+}
+
 func main() {
-	fmt.Println("Hello, Go!")
+	scores := []float64{80, 90, 75, 95}
+
+	result, err := average(scores)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Printf("Average: %.2f\n", result)
 }
